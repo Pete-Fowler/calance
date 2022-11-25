3ccd0b02002ea6acf22f89459bbaae89877195d7
Pete Fowler <fowler.pete@gmail.com>
2022-11-25 14:58:57 -0700

    Edit README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# calance
+# Description
+
+This is a simple script using JavaScript and Node.js. It will fetch Bootstrap release data via the GitHub
+API from `https://api.github.com/repos/twbs/bootstrap/releases`, and then store the requested data - created date, tag name, and download url - in a .csv file. The .csv file will be titled bootstrap-releases.csv, and it will be created at the root of the repository folder.
+
+# Instructions
+
+- Fork and clone this repository.
+- Change directories into the repository folder with `cd calance`.
+- Run the `npm install` terminal command (required for the 'node-fetch' library that is used).
+- Run the `node index.js` terminal command to run the script.
+- Open the `bootstrap-releases.csv` file to view the results.
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import fetch from "node-fetch";
+import fs from "fs";
 
-let data = [];
+// Create empty array to push rows of data into with first row being column names
+let data = [["created_at", "tag_name", "download_url"]];
 
+// Fetch from GitHub API for bootstrap releases
 fetch(`https://api.github.com/repos/twbs/bootstrap/releases
 `).then((r) => {
   if (r.ok) {
     r.json().then((results) => {
-      data.push(["created_at", "tag_name", "download_url"]);
       results.forEach((obj) => {
         data.push([
           obj.created_at,
@@ -14,7 +16,20 @@ fetch(`https://api.github.com/repos/twbs/bootstrap/releases
           obj.assets[0].browser_download_url,
         ]);
       });
-      console.log(data);
+
+      // Parse array into CSV format - first join joins rows into a string
+      // with commas. The second join turns the outer array into a string
+      // separated by new lines
+      const csv = data.map((row) => row.join(",")).join("\n");
+
+      // Use node.js fs.writeFile API to write the data to a .csv
+      // Here we specify the path and filename, then what is being
+      // written (the string in the csv variable), and then a callback
+      // to handle any errors by printing them with console.log() or "done"
+      // if no errors
+      fs.writeFile("./bootstrap-releases.csv", csv, (err) => {
+        console.log(err || "done");
+      });
     });
   } else {
     r.json().then((err) => console.log(err));
